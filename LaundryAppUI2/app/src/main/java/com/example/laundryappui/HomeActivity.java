b2a862e1241d264c6ef644451c1d6b83fe154767
mmdc-adambelda <lr.arbelda@mmdc.mcl.edu.ph>
2025-04-03 20:50:44 +0800

    Updated home Activity java files

--- LaundryAppUI2/app/src/main/java/com/example/laundryappui/HomeActivity.java
+++ LaundryAppUI2/app/src/main/java/com/example/laundryappui/HomeActivity.java
@@ -1,14 +1,40 @@
 package com.example.laundryappui;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class HomeActivity extends AppCompatActivity {
+
+    Button btn;
+    TextView tv;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        btn = findViewById(R.id.button2);
+        tv = findViewById(R.id.textView3);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    startActivity(new Intent(HomeActivity.this,ProfileActivity2.class));
+
+                }
+            }
+        );
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomeActivity.this,ProfileActivity2.class));
+            }
+        });
     }
 }
